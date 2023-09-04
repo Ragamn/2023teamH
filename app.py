e14be897d267b872589ed4f5297a64fe10020491
Fujiwara <113399284+useruser0123@users.noreply.github.com>
2023-09-04 14:36:16 +0900

    投稿管理
    投稿削除機能の追加

--- app.py
+++ app.py
@@ -8,3 +8,32 @@ app.secret_key = ''.join(random.choices(string.ascii_letters,k=256))
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
+
+
+
+
+
+#投稿削除機能
+##データベースモデルに変更
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    deleted = db.Column(db.Boolean, default=False)  # 削除フラグ
+
+##投稿の削除ビューの更新
+@app.route('/delete_post/<int:post_id>', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    
+    # 投稿のユーザーIDがログインユーザーのIDと一致する場合にのみ削除を許可
+    if post.user_id == current_user.id:
+        post.deleted = True  # 削除フラグを立てる
+        db.session.commit()
+    
+    return redirect(url_for('view_posts'))
+
