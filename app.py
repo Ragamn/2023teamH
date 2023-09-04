49b85ac1ad517415446c64f0f650dc1f15cf29b7
showkubota <s.kubota.sys22@morijyobi.ac.jp>
2023-09-04 12:34:53 +0900

    ログインと投稿作成

--- app.py
+++ app.py
@@ -41,5 +41,33 @@ def register_exe():
     error = '登録に失敗しました。'
   return render_template('register.html',error=error)
 
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/login_exe',methods=['POST'])
+def login_exe():
+  accountname = request.form.get('accountname')
+  password = request.form.get('password')
+  
+  # ログイン判定
+  if db.login(accountname, password):
+    session['user'] = True # session にキー：'user', バリュー:True を追加
+    session.permanent = True # session の有効期限を有効化
+    app.permanent_session_lifetime = timedelta(minutes=30)# session の有効期限を5 分に設定
+    if accountname == 's.kubota.sys22@morijyobi.ac.jp':
+      return redirect(url_for('login'))
+    else:
+      return render_template('home.html')
+  else :
+    error = 'ログインに失敗しました。'
+    # dictで返すことでフォームの入力量が増えても可読性が下がらない。
+    input_data = {
+      'accountname':accountname,
+      'password':password
+      }
+    return render_template('index.html',error=error,data=input_data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
