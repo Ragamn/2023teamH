75b408a70c17d28bc5d7a738334d6a4aaa67b0d1
hakkin-1219 <scriptcommand1219@gmail.com>
2023-09-06 10:24:37 +0900

    完成

--- analysis.py
+++ analysis.py
@@ -1,10 +1,14 @@
 from textblob import TextBlob
 import nltk
 from google.cloud import translate_v2 as translate
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+if not nltk.data.find('tokenizers/punkt'):
+    nltk.download('punkt')
 
-# 俺のサービスアカウントキー
+# 'averaged_perceptron_tagger' リソースがインストールされているか確認
+if not nltk.data.find('taggers/averaged_perceptron_tagger'):
+    nltk.download('averaged_perceptron_tagger')
+
+# 誰かのサービスアカウントキー
 path = './rugged-alcove-398101-c4cdeb865efd.json'
 # クライアントの初期化
 translate_client = translate.Client.from_service_account_json(path)
@@ -12,31 +16,29 @@ translate_client = translate.Client.from_service_account_json(path)
 
 def translate_text(text, target_language):
     result = translate_client.translate(text, target_language=target_language)
-    return result['input'], result['translatedText']
+    return result['translatedText']
 
 
 def emotion_analysis(text):
-    blob = TextBlob(text)
+    # 言語検出
     result = translate_client.detect_language(text)
-    print(f"lang={result}")
-    if result["language"] == 'ja':
-        print('ok')
-        tags = blob.tags
+    # 英語でなければ翻訳
+    if result["language"] != 'en':
+        translated_text = translate_text(text, "en")
+        blob = TextBlob(translated_text)
         senti = blob.sentiment
         pol = senti.polarity
         Subjectivity = senti.subjectivity
-        senti_str = f"Polarity: {senti.polarity}, Subjectivity: {senti.subjectivity}"
 
-        # for tag in tags:
-        #     print(f"{tag[0]} ({tag[1]})")
-
-        return senti
-    return 0
-
-
-print(emotion_analysis("こんにちは。お元気ですか。"))
-
-# input_text = "Hello, how are you?"
-# target_language = "ja"
-# input_text, translated_text = translate_text(input_text, target_language)
-# print(translated_text)
+        return pol, Subjectivity
+    else:
+        # Textblobオブジェクトに変換
+        blob = TextBlob(text)
+        # 感情分析
+        senti = blob.sentiment
+        # 極性。マイナスならネガティブ、プラスならポジティブ
+        polarity = senti.polarity
+        # 主観性。1に近い程主観的
+        Subjectivity = senti.subjectivity
+        # それぞれは数値として返ってくる
+        return polarity, Subjectivity
